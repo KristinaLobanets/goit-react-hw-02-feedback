08dfdb67672d199c7dc98e72aa00126cec7a2a13
Kristina Lobanets <kristina.malitowska@gmail.com>
2020-08-12 20:27:26 +0300

    i can

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Title from "../components/titlle/title";
 import FeedbackOptions from "../components/feedback/feedback";
 import Statistik from "../components/statistik/statistik";
+import Notification from "../components/Notification/Notification";
 
 class App extends Component {
   state = {
@@ -10,27 +11,29 @@ class App extends Component {
     bad: 0,
   };
 
-  good = this.state.good;
-  neutral = this.state.neutral;
-  bad = this.state.bad;
+  clickGood = () =>
+    this.setState((preState) => ({
+      good: preState.good + 1,
+    }));
 
-  clickGood = (good) => {
-    return good + 1;
-  };
+  clickNeutral = () =>
+    this.setState((preState) => ({
+      neutral: preState.neutral + 1,
+    }));
 
-  clickNeutral = (neutral) => {
-    return neutral + 1;
-  };
-  clickBad = (bad) => {
-    return bad + 1;
-  };
+  clickBad = () =>
+    this.setState((preState) => ({
+      bad: preState.bad + 1,
+    }));
 
-  countTotalFeedback = (good, neutral, bad) => {
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
     return good + neutral + bad;
   };
 
-  countPositiveFeedbackPercentage = (good, countTotalFeedback) => {
-    return (countTotalFeedback / good) * 100;
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    return Math.round((good * 100) / this.countTotalFeedback());
   };
 
   render() {
@@ -40,14 +43,22 @@ class App extends Component {
     return (
       <>
         <Title />
-        <FeedbackOptions />
-        <Statistik
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          countTotal={this.countTotalFeedback()}
-          countPercentage={this.countPositiveFeedbackPercentage()}
+        <FeedbackOptions
+          clickGood={this.clickGood}
+          clickNeutral={this.clickNeutral}
+          clickBad={this.clickBad}
         />
+        {this.countTotalFeedback() > 0 ? (
+          <Statistik
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            countTotal={this.countTotalFeedback()}
+            countPercentage={this.countPositiveFeedbackPercentage()}
+          />
+        ) : (
+          <Notification />
+        )}
       </>
     );
   }
---
+++ src/components/Notification/Notification.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Notification = () => <h2>No feedback given</h2>;
+
+export default Notification;
